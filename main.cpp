20ac185b121691bce21f7c68d0906a2afddef252
Jabari Crenshaw <jcrenshaw999@gmail.com>
2023-07-20 19:03:28 -0700

    aughh

--- main.cpp
+++ main.cpp
@@ -25,32 +25,33 @@ public:
     };
 
     void update(float d_t){
+
         if(this->getPos().y >= HEIGHT - 50){
-            this->setForce(Pair_2{ this->force.x, 0 });
             this->setAcc(Pair_2 {this->acc.x, 0 });
             this->setVel(Pair_2 { this->vel.x, 0 });
             this->setPos(Pair_2 {this->pos.x, HEIGHT - 50 });
+            printf("Force: %f   Accel: %f   Vel: %f     Pos: %f     Time: %f\n", this->force.y, this->acc.y, this->vel.y, this->pos.y, d_t);
             return;
         }
 
         //this->setForce( Pair_2{ this->force.x, this->mass * f_gravity });
         this->setAcc(Pair_2 { this->force.x, this->force.y / this->mass });
         this->setVel(Pair_2 { this->vel.x, this->vel.y + this->acc.y * d_t });
-        this->setPos(Pair_2 { this->pos.x, this->vel.y + 0.5f * this->acc.y * d_t * d_t});
+        this->setPos(Pair_2 { this->pos.x, this->pos.y + this->vel.y + 0.5f * this->acc.y * d_t * d_t});
 
-        //printf("Force: %f   Accel: %f   Vel: %f     Pos: %f\n", this->force.y, this->acc.y, this->vel.y, this->pos.y);
+        printf("Force: %f   Accel: %f   Vel: %f     Pos: %f     Time: %f\n", this->force.y, this->acc.y, this->vel.y, this->pos.y, d_t);
+
     }
 
     void applyForce(Pair_2 force, float d_t){
         // F = ma
-        /*
-        this->setForce(Pair_2{ this->force.x, 0 });
-        this->setAcc(Pair_2 { this->acc.x, 0 });
-        this->setVel(Pair_2 { this->vel.x, 0 });
-        this->setPos(Pair_2 { this->pos.x, 0 });
-        */
 
-        std::cout << "Apply: "<< this->getPos().x << ' ' << this->getPos().y << std::endl;
+        this->setForce(Pair_2{ this->force.x, this->mass * f_gravity });
+        this->setAcc(Pair_2 { this->force.x, this->force.y / this->mass });
+        this->setVel(Pair_2 { this->vel.x, 0 });
+        this->setPos(Pair_2 { this->pos.x, HEIGHT/2 });
+
+        //std::cout << "Apply: "<< this->force.y << std::endl;
     }
 
     Pair_2 getPos(){ return this->pos; }
@@ -60,7 +61,7 @@ public:
 
 
 private:
-    float f_gravity = 9.807f;
+    float f_gravity = 9.80665f;
     float mass;
     Pair_2 pos;
     Pair_2 acc;
@@ -92,13 +93,15 @@ int main(int argc, char** argv){
         case STATE_CONTINUE:
 
             ClearBackground(DARKBROWN);
-            time += GetFrameTime();
+
             for(size_t i = 0; i < objects.size(); ++i){
-                objects[i].update(time);
+
                 if(IsKeyDown(KEY_SPACE)){
                     objects[i].applyForce(Pair_2 {0, 50}, time);
                     //std::cout << objects[i].getForce().x << ' ' << objects[i].getForce().y << std::endl;
                     //std::cout << objects[i].getPos().x << ' ' << objects[i].getPos().y << std::endl;
+                }else{
+                    objects[i].update(GetFrameTime());
                 }
                 DrawCircle(objects[i].getPos().x, objects[i].getPos().y, 50, RED);
                 //printf("Force: %f   Accel: %f   Vel: %f     Pos: %f\n", objects[i].getForce().y, objects[i].getAcc().y, objects[i].getVel().y, objects[i].getPos().y);
